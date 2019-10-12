38201dd9358f75cda0560bd4876f6cd2dd7ae0af
Roma <romanofkin@gmail.com>
2019-10-12 13:37:11 -0700

    Separated Tests for Binary and Linear Search

--- test/algorithms/BinarySearchTest.java
+++ test/algorithms/BinarySearchTest.java
@@ -3,16 +3,7 @@ package algorithms;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class SearchTest {
-
-    @Test
-    public void linearSearchTest() {
-        int[] array = {1,2,3,4,5};
-
-        assertEquals(2, Search.linearSearch(array, 3));
-        assertEquals(0, Search.linearSearch(array, 1));
-        assertEquals(-1, Search.linearSearch(array, 9));
-    }
+public class BinarySearchTest {
 
     @Test
     public void binarySearchTest() {
